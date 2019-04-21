67a4479f9190b6f572300be322782e53e2a88709
xcAutoTest <359426102@qq.com>
2019-04-21 20:22:51 +0800

    第一次同步

--- DataUpdate/FrmMain.cs
+++ DataUpdate/FrmMain.cs
@@ -1831,7 +1831,7 @@ namespace DataUpdate
                                 dr["检验次数"] = dt_test_status.Rows[0]["JYCS"].ToString();
                                 dr["检验时间"] = dt_test_status.Rows[0]["JCSJ"].ToString();
                                 dr["检测方法"] = dt_test_status.Rows[0]["JCFF"].ToString();
-                                dr["上传成功时间"] = System.DateTime.Now.ToString();
+                                dr["上传成功时间"] = System.DateTime.Now.ToString(); 
                                 dt_AlreadyUpload.Rows.Add(dr);
                                 carinfobd.DeleteCarTestStatus(jylsh, jycs);
                                 carinfobd.deleteCarInWaitlist(jylsh, jycs);
